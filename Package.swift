bba0f08dba8de37ba4fd720d98378b706c887694
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2022-12-29 12:45:02 +0100

    initial package created

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SyncRepo",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.3.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "SyncRepo",
+            dependencies: ["SyncRepoCore"]
+        ),
+        .target(name: "SyncRepoCore")
+    ]
+)
