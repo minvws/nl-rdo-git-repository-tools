4a0a1a6e0b62a1cc9053bf4c2e49fc640ca1f997
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-01-06 14:02:15 +0100

    Improve docs

--- Sources/RepoToolsCore/Git.swift
+++ Sources/RepoToolsCore/Git.swift
@@ -10,7 +10,6 @@ import struct ShellOut.ShellOutError
 
 public struct Git {
 	
-	// i.e. module `ShellOut`'s `shellOut()` function.
 	private let provider: (
 		String, // to:
 		[String], // arguments:
@@ -19,7 +18,8 @@ public struct Git {
 		FileHandle?, // outputHandle:
 		FileHandle? // errorHandle:
 	) throws -> String
-	
+		
+	/// - Parameter provider: i.e. the `shellOut()` function from module `ShellOut`. See ShellOut.shellOut() for more info.
 	public init(provider: @escaping (String, [String], String, Process, FileHandle?, FileHandle?) throws -> String) {
 		self.provider = provider
 	}
@@ -43,10 +43,12 @@ public struct Git {
 		try shellOut(to: "git", arguments: ["lfs push", remote.name, "--all"], at: workingDirectory.path)
 	}
 	
+	/// - Returns: Whether the working directory has any changes, according to git.
 	public func workingDirectoryIsPorcelain(_ workingDirectory: URL) throws -> Bool {
 		return try shellOut(to: "git", arguments: ["status --porcelain"], at: workingDirectory.path).isEmpty
 	}
 	
+	/// - Returns: Whether the git repo in the given directory has a `remote` matching the passed parameter.
 	public func hasRemote(remote: Remote, workingDirectory: URL) throws -> Bool {
 		do {
 			return try !shellOut(to: "git", arguments: ["config remote.\(remote.name).url"], at: workingDirectory.path).isEmpty
@@ -57,17 +59,21 @@ public struct Git {
 		}
 	}
 	
+	/// `git remote add [given remote]`
 	public func addRemote(remote: Remote, workingDirectory: URL) throws {
 		_ = try shellOut(to: "git", arguments: ["remote add \(remote.name) git@github.com:\(remote.repo.path)"], at: workingDirectory.path).isEmpty
 		print("➕ Remote `\(remote.name)` added at github: `\(remote.repo.path)`")
 	}
 	
+	/// Fetches the given remote, also fetching LFS.
 	public func fetchRepo(remote: Remote, workingDirectory: URL) throws {
 		print("🔄 Ensuring we are in sync with `\(remote.name)`")
 		try shellOut(to: "git", arguments: ["fetch \(remote.name)"], at: workingDirectory.path)
 		try shellOut(to: "git", arguments: ["lfs fetch \(remote.name) --all"], at: workingDirectory.path)
 	}
 	
+	/// Create a branch based on `main` with a timestamp branch name.
+	/// - Returns: the created Branch.
 	public func createSyncBranch(workingDirectory: URL, now: Date = Date()) throws -> Branch {
 		// "20221230-111743"
 		let timestamp = {
@@ -84,22 +90,28 @@ public struct Git {
 		return Branch(name: branchName)
 	}
 	
-	public func pushAllReleaseTags(remote: Remote, workingDirectory: URL) throws  {
+	/// Pushes all the local tags to the given remote
+	/// - Parameters:
+	///   - remote: where we're pushing the tags to
+	///   - workingDirectory: the directory containing a git repo
+	///   - matchingGrepPatterns: an array of `grep -e` patterns which tags must conform to at-least-one of to be included in what's pushed.
+	///   - strippingGrepPattern: a `grep -v` pattern which - if matched - will exclude a tag.
+	public func pushAllReleaseTags(remote: Remote, workingDirectory: URL, matchingGrepPatterns: [String]? = nil, strippingGrepPattern: String? = nil) throws  {
 		
 		var commandFragments: [String] = []
 		
 		// get all hash/tag in pairs like "62578f6 refs/tags/2.1.3-Holder"
 		commandFragments += [#"git show-ref --tags"#]
-		// Match only those that don't appear on `public-repo`
+		// Match only those that don't appear on `remote`
 		commandFragments += [#"grep -v -F "$(git ls-remote --tags \#(remote.name) | grep -v '\^{}' | cut -f 2)""#]
-
+		
 		// Match only those that fit a `grep -e` pattern:
 		if let matchingGrepPatterns, !matchingGrepPatterns.isEmpty {
-
+			
 			let matchGrepCommand = matchingGrepPatterns
 				.map { #" -e "\#($0)""# }
 				.joined()
-
+			
 			// grep -e "Holder-" -e "Verifier-"
 			commandFragments += [#"grep\#(matchGrepCommand)"#]
 		}
@@ -108,7 +120,7 @@ public struct Git {
 		if let strippingGrepPattern, !strippingGrepPattern.isEmpty {
 			commandFragments += [#"grep -v "\#(strippingGrepPattern)""#]
 		}
-
+		
 		// Get the tag name, drop the hash.
 		commandFragments += [#"cut -f2 -d " ""#]
 		// Finally push each tag to `public-repo` remote.
